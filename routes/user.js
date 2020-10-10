7ba447481e4a9493797b089b010ee3e7b841ba90
Arathy Sadan <arathysadan@gmail.com>
2020-10-10 14:50:28 +0530

    login and create account page created and db connected

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers');
+const userHelpers = require('../helpers/user-helpers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,5 +11,21 @@ router.get('/', function(req, res, next) {
   })
 
 });
+/*create account page*/
+router.get('/signup',(req,res)=>{
+res.render('user/signup',{user:true})
+})
+
+/*get login page*/
+router.get('/login',(req,res)=>{
+res.render('user/login',{user:true});
+})
+
+router.post('/signup',(req,res)=>{
+  /*console.log(req.body)*/
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
+})
 
 module.exports = router;
