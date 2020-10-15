1e10eb7fce3254693459393933e57373ed583d89
Arathy Sadan <arathysadan@gmail.com>
2020-10-15 20:18:00 +0530

    edit and delete actions in admin panel

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,12 +1,15 @@
 var db = require('../config/connection')
 var collection = require('../config/collections');
 const { PRODUCT_COLLECTION } = require('../config/collections');
+const collections = require('../config/collections');
+const { response } = require('../app');
 var objectId = require('mongodb').ObjectID
 module.exports = {
 
     addProduct:(product,callback)=>{
        
-        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product)
+        .then((data)=>{
           
             callback(data.ops[0]._id);
         })
@@ -14,18 +17,47 @@ module.exports = {
     
     getAllProducts:()=>{
         return new Promise(async(resolve,reject)=>{
-            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION)
+            .find().toArray()
             resolve(products)
         })
     },
 
     deleteProduct:(proId)=>{
         return new Promise((resolve,reject)=>{
-            db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:objectId(proId)}).then((response)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .removeOne({_id:objectId(proId)}).then((response)=>{
                 resolve(response)
             })
         })
 
+    },
+    
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collections.PRODUCT_COLLECTION)
+            .findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collections.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Category:proDetails.Category,
+                    Price:proDetails.Price,
+                    Description:proDetails.Description
+                }
+            }).then((response)=>{
+                resolve()
+            })
+
+
+        })
     }
+
     
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const fileUpload = require('express-fileupload');
 const { Db } = require('mongodb');
-const { render } = require('../app');
+const { render, response } = require('../app');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers');
 
@@ -36,6 +36,22 @@ router.get('/delete-product/:id',(req,res)=>{
   let proId=req.params.id
   productHelpers.deleteProduct(proId).then((response)=>{
     res.redirect('/admin/')
+  })
+})
+router.get('/edit-product/:id',async(req,res)=>{
+  let product = await productHelpers.getProductDetails(req.params.id)
+  console.log(product)
+  res.render('admin/edit-product',{admin:true,product})
+
+})
+router.post('/edit-product/:id',(req,res)=>{
+  let id = req.params.id
+  productHelpers.updateProduct(req.params.id,req.body).then(()=>{
+    res.redirect('/admin')
+    if(req.files.Image){
+      let image = req.files.Image
+      image.mv('./public/product-images/'+id+'.jpg')
+    }
   })
 })
 
